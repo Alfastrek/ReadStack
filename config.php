5ae8a54b406b0e7d04e7f766d2466fba08cb21bc
Aradhya Shukla <aradhya1703@gmail.com>
2024-08-08 21:35:30 +0530

    Configuration updated for Azure Mysql

--- config.php
+++ config.php
@@ -1,5 +1,16 @@
 <?php
+// Retrieve database connection details from environment variables
+$dbHost = getenv('AZURE_MYSQL_HOST');
+$dbUser = getenv('AZURE_MYSQL_USERNAME');
+$dbPass = getenv('AZURE_MYSQL_PASSWORD');
+$dbName = getenv('AZURE_MYSQL_DBNAME');
+$dbPort = getenv('AZURE_MYSQL_PORT'); // Optional, if you need to specify a port
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+// Create connection
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
 
+// Check connection
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
 ?>
