ab6749cfe05ff145b788f67853e976d6399f7264
Aradhya Shukla <aradhya1703@gmail.com>
2024-08-09 01:15:25 +0530

    Deployment update

--- config.php
+++ config.php
@@ -5,5 +5,4 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Connected successfully";
 ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// public/index.php
+
+// Redirect to home.php
+header("Location: home.php");
+exit();
+?>
