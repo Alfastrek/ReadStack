39bd63f3e77c282169880296c955b64e54dc84ec
Aradhya Shukla <aradhya1703@gmail.com>
2024-08-09 00:08:25 +0530

    folder restructure

--- config.php
+++ config.php
@@ -1,16 +1,6 @@
+
 <?php
-// Retrieve database connection details from environment variables
-$dbHost = getenv('AZURE_MYSQL_HOST');
-$dbUser = getenv('AZURE_MYSQL_USERNAME');
-$dbPass = getenv('AZURE_MYSQL_PASSWORD');
-$dbName = getenv('AZURE_MYSQL_DBNAME');
-$dbPort = getenv('AZURE_MYSQL_PORT'); // Optional, if you need to specify a port
 
-// Create connection
-$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
+$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
 
-// Check connection
-if (!$conn) {
-    die('Connection failed: ' . mysqli_connect_error());
-}
 ?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// public/index.php
+
+// Redirect to shop.php by default
+header("Location: home.php");
+exit();
