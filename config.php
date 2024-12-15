db5669dce883e01588d6e9330cecd935b6272083
Aradhya Shukla <aradhya1703@gmail.com>
2024-12-15 18:06:46 +0530

    config update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect('readstack.mysql.database.azure.com', 'alfastrek', 'Abhinav@1703', 'shop_db', 3306);
-
+// $conn = mysqli_connect('readstack.mysql.database.azure.com', 'alfastrek', 'Abhinav@1703', 'shop_db', 3306);
+$conn = mysqli_connect('localhost', 'root', '', 'shop_db', 3306);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
